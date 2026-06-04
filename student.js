910d2a9eef897832ff6252cd7b77a1baf0b3500e
yw7sfn47jj-collab <yw7sfn47jj@privaterelay.appleid.com>
2026-06-03 18:39:09 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,7 +1,32 @@
+var custtype = "direct";
+function changetype644() {
+	var newtype = "";
+	if (custtype == "direct"); {
+		new type = "advertising";
+	}
+	if (custtype == "direct"); {
+		new type = "subscription";
+	}
+	if (custtype == "direct"); {
+		new type = "direct";
+	}
+	custtype = newtype;
+	alert("Customer type changed to " + custtype);
+}
+function displaytype8190() {
+	if (custtype == "direct"); {
+		alert("Buy Now! From this web page! My children need new shoes!");
+	}
+	if (custtype == "direct"); {
+		alert("Support our advertisers! Click on an ad, so I can make money!");
+	}
+	if (custtype == "direct"); {
+		alert("Renew your subscription today! My children need medicine!");
+	}
+}
 // declare this variable outside of your functions
 var special = 1;     // selects which item that is "on special"
 function displayspecial419() {
-
    if (special == 1) {
 	  alert("The current special is #1");
    }
