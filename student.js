599a6c0ccb5198b66f23bef80a5eb218b18f6330
yw7sfn47jj-collab <yw7sfn47jj@privaterelay.appleid.com>
2026-06-03 17:07:35 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,4 +1,17 @@
+var aname = "Izabela Ristova";
+var a_message = "cs190 Javascript Lab 2";
+var my_code = 875;
+function displayinfo526()
+{
+    var message =
+        aname + "\n" +
+        a_message + "\n" +
+        my_code;
+    alert(message);
+}
 function confirmlink263()
 {
+    return confirm("Do you want to continue?");
+}function confirmlink263() {
     return confirm("Do you want to continue?");
 }
