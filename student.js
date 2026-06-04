f739abd75d5d9672aa389c181ddba8ab4d608a00
yw7sfn47jj-collab <yw7sfn47jj@privaterelay.appleid.com>
2026-06-03 17:47:50 -0700

    Update student.js

--- student.js
+++ student.js
@@ -3,7 +3,7 @@ var a_message = "cs190 Javascript Lab 2";
 var my_code = 875;
 
 function displayinfo526() {
-    alert(aname + "\n" + a_message + "\n" + my_code);
+    alert(aname + "\\n" + a_message + "\\n" + my_code);
 }
 function confirmlink263() {
     return confirm("Do you want to continue?");
