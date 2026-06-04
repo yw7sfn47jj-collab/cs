69b93b467a3d29b8e01e503a0b5f6a246491abb1
yw7sfn47jj-collab <yw7sfn47jj@privaterelay.appleid.com>
2026-06-03 18:06:55 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,3 +1,21 @@
+// declare this variable outside of your functions
+var special = 1;     // selects which item that is "on special"
+function displayspecial419() {
+
+   if (special == 1) {
+	  alert("The current special is #1");
+   }
+   if (special == 2) {
+	  alert("The current special is #2");
+   }
+   if (special == 3) {
+	  alert("The current special is #3";
+   }
+   special = special + 1;
+   if (special > 3) {
+	 special = 1;  // wrap around
+   }
+}
 var aname = "Izabela Ristova";
 var a_message = "cs190 Javascript Lab 2";
 var my_code = 875;
